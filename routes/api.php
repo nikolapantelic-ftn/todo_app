be3b31ae406f8132afad707af77a2fecea87fdc9
nikola <nikolapantelic434@gmail.com>
2018-12-14 14:31:30 +0100

    JWT auth fix

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
+
 Route::group([
 
     'middleware' => 'api',
